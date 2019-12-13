6900485c474ccf047db18af33134d542c427fd2e
gleb <gleby-hleby@inbox.ru>
2019-12-14 01:31:31 +0300

    Scripted auto-commit on change (2019-12-14 01:31:31) by gitwatch.sh

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,8 +1,15 @@
-import
-
-import akka.stream.StreamRefMessages;
+import akka.NotUsed;
+import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 
 import java.io.IOException;
+import java.util.concurrent.CompletionStage;
 
 public class Launcher {
     public static void main(String[] args) throws IOException {
