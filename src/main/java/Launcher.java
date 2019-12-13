9103b8267711fa365672f6e89e45df0cd7a7a1b3
gleb <gleby-hleby@inbox.ru>
2019-12-14 01:33:24 +0300

    Scripted auto-commit on change (2019-12-14 01:33:24) by gitwatch.sh

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -17,6 +17,6 @@ public class Launcher {
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
 
-        
+        final Server pingServer = new Server(system);
     }
 }
