d5902011527769c8bf2de12f4874eed12fb58ccc
gleb <gleby-hleby@inbox.ru>
2019-12-14 00:47:39 +0300

    Scripted auto-commit on change (2019-12-14 00:47:39) by gitwatch.sh

--- src/main/java/Request.java
+++ src/main/java/Request.java
@@ -2,7 +2,7 @@ public class Request {
     public String testUrl;
     public int count;
 
-    public Request(String TestUrl, int count){
+    public Request(String testUrl, int count){
         this.testUrl = testUrl;
         this.count = count;
     }
